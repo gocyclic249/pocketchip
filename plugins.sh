594551b7bdb3d30a2537c3b9a2a200cbddd275eb
gocyclic249 <gocyclic249@gmail.com>
2016-03-28 09:44:25 -0400

    updated install script

--- plugins.sh
+++ plugins.sh
@@ -2,10 +2,28 @@
 
 sudo apt-get update && sudo apt-get upgrade 
 
-sudo apt-get install vim tmux zsh
+sudo apt-get install emacs tmux zsh texlive-full pandoc stow
 
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+chsh -s /bin/zsh
 
-sh ./install.sh
+rm -rf ~/.emacs.d
+rm -rf ~/.emacs
+
+
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > neo.sh
+
+sh ./neo.sh
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm -rf ~/.zshrc
+
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+cd ~/dotfiles
+
+stow vim
+stow spacemacs
+stow zsh
+stow Xresources
